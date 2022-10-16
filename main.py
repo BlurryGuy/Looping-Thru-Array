cdbfb01626f8abfa6a237374ea31f00319100c9b
Blurry <114656831+BlurryGuy@users.noreply.github.com>
2022-10-16 13:14:11 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+my_array = ["how", "are", "hello", "you"]
+
+for string in my_array:
+    if string == "hello":
+       print("Hello was found!")
+       break
+    else:
+        print("Hello was not found")
+input()
